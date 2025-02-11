8d6a02f3c8d396e1a603d324d83d629667e035fc
Hawa Maria <hawamaria33@gmail.com>
2025-02-11 11:43:23 +0300

    update

---
+++ script/DeployTwitter.s.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Script, console} from "forge-std/Script.sol";
+import {Twitter} from "../src/Twitter.sol";
+
+contract CounterScript is Script {
+    
+}
---
+++ test/TwitterTest.t.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {DeployTwitter} from "../script/DeployTwitter.s.sol";
+contract TwitterTest is Test {
+
+}
